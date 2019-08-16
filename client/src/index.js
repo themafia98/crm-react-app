257044eea7386d48554bb257701a704661265428
Pavel Petrovich <gaara33377@gmail.com>
2019-08-16 17:07:55 +0300

    normalize.css

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
-
+import 'normalize.css';
 ReactDOM.render(
     <BrowserRouter>
         <App />
