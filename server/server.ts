f4382109773ac5a3807e87df8a3919b3227e064d
Pavel Petrovich <gaara33377@gmail.com>
2019-08-16 23:22:01 +0300

    server types

--- server/server.ts
+++ server/server.ts
@@ -20,21 +20,15 @@ let port:string = process.env.PORT || '3001';
 app.set('port', port);
 
 let server = http.createServer(app);
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req:any, res:any, next:any):void {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err:any, req:any, res:any, next:any):void {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -45,23 +39,19 @@ app.use(function(err, req, res, next) {
 });
 
 
-function onError(error) {
+function onError(error:any):void {
   if (error.syscall !== 'listen') {
     throw error;
   }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+      console.error(port + ' requires elevated privileges');
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+      console.error(port + ' is already in use');
       process.exit(1);
       break;
     default:
@@ -69,17 +59,13 @@ function onError(error) {
   }
 }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+function onListening():void {
+  debug('Listening on ' + server.address());
 }
 
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
 module.exports = app;
