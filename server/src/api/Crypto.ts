bafeeac775a2bbfecbea7675f2efb101d87d9560
Pavel Petrovich <gaara33377@gmail.com>
2019-08-20 18:37:00 +0300

    crypto init

---
+++ server/src/api/Crypto.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt';
+
+
+class Crypto {
+    
+}
+
+// // пароль пользователя
+// var passwordFromUser = "3PdertE35710h";
+ 
+// // создаем соль
+// var salt = bcrypt.genSaltSync(10);
+ 
+// // шифруем пароль
+// var passwordToSave = bcrypt.hashSync(passwordFromUser, salt)
+ 
+// // выводим результат
+// console.log(passwordToSave);
+// console.log(`${bcrypt.hashSync('test_user_pass', salt) == passwordToSave}`);
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,58 +3,35 @@ import AppNamespace from './app';
 import rateLimit from 'express-rate-limit';
 import path from 'path';
 import fs from 'fs';
-import {debug, log} from './logger/logModule';
+import {log} from './logger/logModule';
 
+namespace server {
 
-import bcrypt from 'bcrypt';
+  const {app} = AppNamespace;
+  export const port:string = process.env.PORT || '3001';
 
-// пароль пользователя
-var passwordFromUser = "3PdertE35710h";
- 
-// создаем соль
-var salt = bcrypt.genSaltSync(10);
- 
-// шифруем пароль
-var passwordToSave = bcrypt.hashSync(passwordFromUser, salt)
- 
-// выводим результат
-console.log(passwordToSave);
-console.log(`${bcrypt.hashSync('test_user_pass', salt) == passwordToSave}`);
+  if(process.env.NODE_ENV === 'production')
+  app.locals.frontend = new URL('https://themafia98.github.io');
+  else app.locals.frontend = new URL('http://localhost:3000');
 
-
-
-const {app} = AppNamespace;
-const port:string = process.env.PORT || '3001';
-
-if(process.env.NODE_ENV === 'production')
-app.locals.frontend = new URL('https://themafia98.github.io');
-else app.locals.frontend = new URL('http://localhost:3000');
-
-fs.readFile(path.join(__dirname, '/configs', 'limit.json'),'utf-8', (error: Error, config:Buffer) => {
-  app.use(rateLimit(config));  // 15 * 60 * 1000
-});
-    // // error handler
-app.use((err:Error, req:Request, res:Response):void => {
-      // set locals, only providing error in development
-  const today = new Date();
-  const time  = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  const day = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  log.error(`${res.locals.message} / ${day}/${time}`);
-  res.sendStatus(403);
-});
-
-app.set('port', port);
-app.listen(port,() => {
-
+  fs.readFile(path.join(__dirname, '/configs', 'limit.json'),'utf-8', (error: Error, config:Buffer) => {
+    app.use(rateLimit(config));  // 15 * 60 * 1000
+  });
+      // // error handler
+  app.use((err:Error, req:Request, res:Response):void => {
+        // set locals, only providing error in development
     const today = new Date();
     const time  = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     const day = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+      
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    log.error(`${res.locals.message} / ${day}/${time}`);
+    res.sendStatus(403);
+  });
 
-    console.log(`Server listen on ${port} with origin ${app.locals.frontend}`);
-    // debug.info(`Server ${app.get('port')} start in ${day}/${time} with origin ${app.locals.frontend}`);
-
-});
-
+  app.set('port', port);
+  app.listen(port,() => {
+      console.log(`Server listen on ${port} with origin ${app.locals.frontend}`);
+  });
+}
