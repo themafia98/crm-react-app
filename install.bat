2dc5694d81e7c1fca16565582778eaf30fc313f2
Pavel Petrovich <gaara33377@gmail.com>
2019-09-14 16:37:18 +0300

    install script

--- install.bat
+++ install.bat
@@ -1,12 +1,26 @@
+cls
 @echo off 
 
-set mydrive=C:\ 
-set mypath=Dev\Frontend\crm-react-app\
-cd %mydrive%
-cd %mypath%
+set pathProject = ""
+set /P pathProject=Enter path (Default: C:\Dev\Frontend\crm-react-app\):
+
+if NOT DEFINED pathProject (
+    echo set default path to project C:\Dev\Frontend\crm-react-app\
+    set pathProject=C:\Dev\Frontend\crm-react-app\
+)
+
+cd %pathProject%
 
 cd client
+echo %cd%
+echo Install Client start
 call yarn install
+
 cd ../server
+echo %cd%
+echo Install Server start
 call yarn install
+
+echo Install end.
 pause
+exit
