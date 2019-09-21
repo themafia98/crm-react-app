acb4a3c4efcca5311d8e93408fe2ed7c040adbac
Pavel Petrovich <gaara33377@gmail.com>
2019-09-21 14:37:55 +0300

    admin panel start

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
-import {Request, Response} from 'express';
+import express,{Request, Response} from 'express';
 import AppNamespace from './app';
 import rateLimit from 'express-rate-limit';
 import dotenv from 'dotenv';
 import path from 'path';
 import fs from 'fs';
 
-
+import {WHITELIST} from './utils/const';
 import Security from './api/security';
 import MailNamespace from './api/Mail';
 
@@ -20,13 +20,11 @@ namespace Server {
 
 
   if(process.env.NODE_ENV === 'production')
-  app.locals.frontend = new URL('https://crm-react-app.netlify.com');
-  else app.locals.frontend = new URL('http://localhost:3000');
+    WHITELIST.push('https://crm-react-app.netlify.com');
 
   fs.readFile(path.join(__dirname, '/configs', 'limit.json'),'utf-8', (error: Error, config:Buffer) => {
     app.use(rateLimit(config));  // 15 * 60 * 1000
   });
-
 
       /** @ErrorHandler */
   app.use((err:Error, req:Request, res:Response):void => {
@@ -35,12 +33,12 @@ namespace Server {
             const time  = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
             const day = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
             console.error(err.name);
-            log.error(`${err.name} / ${day}/${time}`);
+            if (err.name) log.error(`${err.name} / ${day}/${time}`);
   });
 
   app.set('port', port);
   const server = app.listen(port,() => {
-      console.log(`Server listen on ${port} with origin ${app.locals.frontend}`);
+      console.log(`Server listen on ${port} with origin whitelist: ${WHITELIST.join("")}`);
       Security.create().then(res => {
         app.locals.sender = new MailNamespace.Sender({
           host: 'smtp.gmail.com',
